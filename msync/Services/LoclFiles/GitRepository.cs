50ab46e563087fe1e7b45b69b0f74db80032b337
hyfree <wangxianqiangcn@foxmail.com>
2024-03-26 22:28:51 +0800

    支持git协议管理本地文件

---
+++ msync/Services/LoclFiles/GitRepository.cs
@@ -0,0 +1,19 @@
+﻿using LibGit2Sharp;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msync.Services.LoclFiles
+{
+	public class GitRepository
+	{
+		public void Init(string path)
+		{
+			string rootedPath = Repository.Init(path);
+		}
+
+	}
+}
